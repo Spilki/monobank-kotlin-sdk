0fa839af70452e84d9022b014b5d1d5bdd3dfce3
damienmiheev <damienmiheev@gmail.com>
2026-02-24 21:41:32 +0200

    Add overload

--- src/main/kotlin/app/spilki/monobank/acquiring/MonobankAcquiringConfig.kt
+++ src/main/kotlin/app/spilki/monobank/acquiring/MonobankAcquiringConfig.kt
@@ -10,11 +10,13 @@ package app.spilki.monobank.acquiring
  * @property cmsName Optional CMS name sent in `X-Cms` header.
  * @property cmsVersion Optional CMS version sent in `X-Cms-Version` header.
  */
-public data class MonobankAcquiringConfig(
-    val token: String,
-    val baseUrl: String = "https://api.monobank.ua",
-    val connectTimeoutMs: Long = 10_000,
-    val readTimeoutMs: Long = 30_000,
-    val cmsName: String? = null,
-    val cmsVersion: String? = null,
-)
+public data class MonobankAcquiringConfig
+    @JvmOverloads
+    constructor(
+        val token: String,
+        val baseUrl: String = "https://api.monobank.ua",
+        val connectTimeoutMs: Long = 10_000,
+        val readTimeoutMs: Long = 30_000,
+        val cmsName: String? = null,
+        val cmsVersion: String? = null,
+    )
